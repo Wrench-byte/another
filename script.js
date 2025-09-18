8bd7a7f376bf12d6a41eb60e771ab76c3288097a
Wrench <jasonbrody23far@gmail.com>
2025-09-18 10:04:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function show(){
+    alert('Реально думала про игрых?)')
+    
+}
+
+function mostrar(){
+    show()
+    setTimeout(parf, 2000 )
+    
+    
+}
+
+function parf() {
+    window.open('pag2.html')
+    window.close()
+}
